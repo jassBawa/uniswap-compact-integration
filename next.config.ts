584a90d2a9aab0b4227abdab3b3e7dd6eb3078da
jassbawa <jasspreetsbawa@gmail.com>
2026-01-03 14:30:53 +0530

    fix: tsconfig changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
+
 };
 
 export default nextConfig;
